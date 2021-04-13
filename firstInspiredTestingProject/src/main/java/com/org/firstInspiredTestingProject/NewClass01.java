68195d4f5555f2260bb9c3150fe1139ce72d4159
joshs <joshs@DESKTOP-38EP2BP.Dlink>
2021-04-13 16:17:14 +0200

    This is our first commit to the Master branch of this project.

---
+++ firstInspiredTestingProject/src/main/java/com/org/firstInspiredTestingProject/NewClass01.java
@@ -0,0 +1,10 @@
+package com.org.firstInspiredTestingProject;
+
+public class NewClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
